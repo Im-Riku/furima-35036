3022851147ec8db7428e4295c72bdc4c1e397051
Im-Riku <rikuchi19@gmail.com>
2021-04-07 10:46:08 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/}
+  validates :last_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/}
+  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]/}
+  validates :last_name_katakana, presence: true, format: { with: /\A[ァ-ヶー-]+\z/}
+  validates :first_name_katakana, presence: true, format: { with: /\A[ァ-ヶー-]+\z/}
+  validates :birthday, presence: true
+
+
+  
+end
