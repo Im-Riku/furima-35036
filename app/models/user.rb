b4d4da8fb3b21a3911338bb4b80bf9135f794b76
Im-Riku <rikuchi19@gmail.com>
2021-04-07 21:59:55 +0900

    再修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   with_options presence: true do
     validates :nickname
-    validates :last_name_kanji, format: { with: /\A[ぁ-んァ-ヶ一-龥]/}
-    validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ヶ一-龥]/}
+    validates :last_name_kanji, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/}
+    validates :first_name_kanji, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]/}
     validates :last_name_katakana, format: { with: /\A[ァ-ヶー-]+\z/}
     validates :first_name_katakana, format: { with: /\A[ァ-ヶー-]+\z/}
     validates :birthday
