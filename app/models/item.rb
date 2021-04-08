174900bc5a10358308ef6eefce5b3f61b928173c
Im-Riku <rikuchi19@gmail.com>
2021-04-08 15:48:08 +0900

    Gemの導入、バリデーション、アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,23 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :delivery_source
+  belongs_to :days_to_ship
+
+  with_options presence: true do
+    validates :item_name
+    validates :explanation
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :delivery_fee_id, numericality: { other_than: 1 }
+    validates :delivery_source_id, numericality: { other_than: 1 }
+    validates :days_to_ship_id, numericality: { other_than: 1 }
+    validates :price
+    validates :image
+  end
 end
